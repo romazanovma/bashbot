8c6e73acde7de7d5bbdd950ba0db5b70f1613e73
romazanovma <poma3ahob@ya.ru>
2021-03-10 14:39:03 +0400

    added changes to src/bashbot/bashbot

--- src/bashbot/bashbot.go
+++ src/bashbot/bashbot.go
@@ -1,64 +1,66 @@
 package main
 
 import (
-    "os"
-    "os/exec"
-    "log"
-    "github.com/go-telegram-bot-api/telegram-bot-api"
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 )
 
+//some changes here
+
 type Commands struct {
-    Cmds []Command `json:"commands"`
+	Cmds []Command `json:"commands"`
 }
 
 type Command struct {
-    Name string `json:"name"`
-    Cmd string `json:"command"`
+	Name string `json:"name"`
+	Cmd  string `json:"command"`
 }
 
+//some changes there
 
 func main() {
-    bot, err := tgbotapi.NewBotAPI(env("TELEGRAM_BOT_TOKEN", ""))
-    if err != nil {
-        log.Panic(err)
-    }
-    log.Printf("Connected to bot API")
+	bot, err := tgbotapi.NewBotAPI(env("TELEGRAM_BOT_TOKEN", ""))
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("Connected to bot API")
 
-    commandsFileName := env("COMMANDS_FILE", "commands.json")
-    file, err := ioutil.ReadFile(commandsFileName)
-    if err != nil {
-        log.Panic(err)
-    }
-    var commands Commands
-    err = json.Unmarshal(file, &commands)
+	commandsFileName := env("COMMANDS_FILE", "commands.json")
+	file, err := ioutil.ReadFile(commandsFileName)
+	if err != nil {
+		log.Panic(err)
+	}
+	var commands Commands
+	err = json.Unmarshal(file, &commands)
 
-    updateConfiguration := tgbotapi.NewUpdate(0)
-    updateConfiguration.Timeout = 60
+	updateConfiguration := tgbotapi.NewUpdate(0)
+	updateConfiguration.Timeout = 60
 
-    updates, err := bot.GetUpdatesChan(updateConfiguration)
+	updates, err := bot.GetUpdatesChan(updateConfiguration)
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
-        if update.Message.IsCommand() {
-            for _, command := range commands.Cmds {
-                if command.Name == update.Message.Command() {
-                    out, _ := exec.Command("sh", "-c", command.Cmd).Output()
-                    msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(out))
-                    msg.ReplyToMessageID = update.Message.MessageID
-                    bot.Send(msg)
-                }
-            }
-        }
+		if update.Message.IsCommand() {
+			for _, command := range commands.Cmds {
+				if command.Name == update.Message.Command() {
+					out, _ := exec.Command("sh", "-c", command.Cmd).Output()
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(out))
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+				}
+			}
+		}
 	}
 }
 
 func env(key, defaultValue string) string {
-    val := defaultValue
-    if envVal := os.Getenv(key); envVal != "" {
-       val=envVal
-    }
-    return val
+	val := defaultValue
+	if envVal := os.Getenv(key); envVal != "" {
+		val = envVal
+	}
+	return val
 }
